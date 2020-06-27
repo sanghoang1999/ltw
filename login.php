22c7fa39407ac0c3c268bcd7d217572738513c88
Duc Trung <sductrung@gmail.com>
2020-06-27 14:55:21 +0700

    fix login and register page

--- login.php
+++ login.php
@@ -1,68 +1,68 @@
 <?php
 // Initialize the session
 session_start();
- 
+
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
     header("location: welcome.php");
     exit;
 }
- 
+
 // Include config file
 require_once "config.php";
- 
+
 // Define variables and initialize with empty values
 $username = $password = "";
 $username_err = $password_err = "";
- 
+
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
- 
+
     // Check if username is empty
     if(empty(trim($_POST["username"]))){
         $username_err = "Please enter username.";
     } else{
         $username = trim($_POST["username"]);
     }
-    
+
     // Check if password is empty
     if(empty(trim($_POST["password"]))){
         $password_err = "Please enter your password.";
     } else{
         $password = trim($_POST["password"]);
     }
-    
+
     // Validate credentials
     if(empty($username_err) && empty($password_err)){
         // Prepare a select statement
         $sql = "SELECT id, username, password FROM users WHERE username = ?";
-        
+
         if($stmt = $mysqli->prepare($sql)){
             // Bind variables to the prepared statement as parameters
             $stmt->bind_param("s", $param_username);
-            
+
             // Set parameters
             $param_username = $username;
-            
+
             // Attempt to execute the prepared statement
             if($stmt->execute()){
                 // Store result
                 $stmt->store_result();
-                
+
                 // Check if username exists, if yes then verify password
-                if($stmt->num_rows == 1){                    
+                if($stmt->num_rows == 1){
                     // Bind result variables
                     $stmt->bind_result($id, $username, $hashed_password);
                     if($stmt->fetch()){
                         if(password_verify($password, $hashed_password)){
                             // Password is correct, so start a new session
                             session_start();
-                            
+
                             // Store data in session variables
                             $_SESSION["loggedin"] = true;
                             $_SESSION["id"] = $id;
-                            $_SESSION["username"] = $username;                            
-                            
+                            $_SESSION["username"] = $username;
+
                             // Redirect user to welcome page
                             header("location: welcome.php");
                         } else{
@@ -82,43 +82,213 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             $stmt->close();
         }
     }
-    
+
     // Close connection
     $mysqli->close();
 }
 ?>
- 
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Login</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
-    </style>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+ <!--bootsstrap cdn-->
+  <link rel="stylesheet"
+  href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css"
+  integrity="sha384-PDle/QlgIONtM1aqA2Qemk5gPOE7wFq8+Em+G/hmo5Iq0CCmYZLv3fVRDJ4MMwEA"
+  crossorigin="anonymous">
+  <!--font awsome cdn-->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
+  integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
+  crossorigin="anonymous">
+  <script type="text/javascript">
+    function toBottom() {
+      window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' })
+    }
+  </script>
+  <link rel="stylesheet" href="css/style.css">
+  <title>Login</title>
 </head>
 <body>
-    <div class="wrapper">
-        <h2>Login</h2>
-        <p>Please fill in your credentials to login.</p>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
-                <label>Username</label>
-                <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
-                <span class="help-block"><?php echo $username_err; ?></span>
-            </div>    
-            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-                <label>Password</label>
-                <input type="password" name="password" class="form-control">
-                <span class="help-block"><?php echo $password_err; ?></span>
+<!--top bar (pt=padding top ,pb=padding bottom) -->
+    <div class="container-fluid bg-dark header-top d-md-block d-none">
+        <div class="container">
+            <div class="row text-light pt-2 pb-2">
+                <div class="col-md-5">
+                    <i class="far fa-envelope" aria-hidden="true"></i>
+                    tiuh@tiuh.corp
+                </div>
+                <div class="col-md-3"></div>
+                <div class="col-md-2"><i class="fa fa-user" aria-hidden="true"> </i> <a style="color: white; text-decoration: none;" href="login.html"> Tài khoản </a></div>
+                <div class="col-md-2"><i class="fa fa-cart-plus" aria-hidden="true"> </i> <a style="color: white; text-decoration: none;" href="buy.html"> Giỏ hàng </a></div>
             </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Login">
+        </div>
+    </div>
+
+
+<!--nav bar-->
+<div class="container-fluid bg-black">
+
+    <nav class="container navbar navbar-expand-lg navbar-dark bg-black">
+            <a class="navbar-brand" href="index.html"><h1>Tiuh</h1></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="index.html">Trang chủ <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="intro.html">Giới thiệu</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Sản Phẩm
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="shoes.html">Giày</a>
+                    <a class="dropdown-item" href="accessories.html">Phụ Kiện</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="sale.html">Giảm giá</a>
+                  </div>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="javascript:toBottom()">Liên hệ</a>
+                </li>
+              </ul>
+              <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+              </form>
             </div>
-            <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
-        </form>
-    </div>    
+          </nav>
+</div>
+
+
+  <div class="container-fluid bg-light-gray">
+
+  <div class="container pt-5">
+
+    <div class="row">
+      <h3>ĐĂNG NHẬP</h3>
+    </div>
+      <div class="row">
+      <div class="underline"></div>
+    </div>
+  </div>
+  <div class="container pt-5">
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+            <label>Username</label>
+            <input type="text" name="username" size=30 class="form-control" value="<?php echo $username; ?>">
+            <span class="help-block"><?php echo $username_err; ?></span>
+        </div>
+        <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+            <label>Password</label>
+            <input type="password" name="password" size=30 class="form-control">
+            <span class="help-block"><?php echo $password_err; ?></span>
+        </div>
+        <div class="form-group">
+            <input type="submit" class="btn btn-primary" value="Login">
+        </div>
+        <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
+    </form>
+  </div>
+
+  </div>
+
+
+
+
+
+  <footer>
+    <div class="container-fluid pt-5 pb-5 bg-dark text-light">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3">
+          <div class="row">
+            <h5>Thử nghiệm</h5>
+          </div>
+          <div class="row mb-4">
+          <div class="underline bg-light" style="width: 50px;"></div>
+          </div>
+
+              <p><i class="fa fa-chevron-right" aria-hidden="true"></i> Đăng kí</p>
+              <p><i class="fa fa-chevron-right" aria-hidden="true"></i> Đăng nhập</p>
+              <p><i class="fa fa-chevron-right" aria-hidden="true"></i> Bảo mật</p>
+              <p><i class="fa fa-chevron-right" aria-hidden="true"></i> Bình luận</p>
+              <p><i class="fa fa-chevron-right" aria-hidden="true"></i> Hướng dẫn</p>
+
+          </div>
+
+
+          <div class="col-md-3">
+            <div class="row">
+            <h5>Bài đăng gần nhất</h5>
+          </div>
+          <div class="row mb-4">
+          <div class="underline bg-light" style="width: 50px;"></div>
+          </div>
+          <div class="row">
+            <div class="media">
+              <img src="img/img-25.jpg" class="img-fluid" alt="media-image">
+              <div class="media-body ml-2">
+                <h6>Joshua Kimmich. Cầu thủ tất cả trong một!</h6>
+              </div>
+            </div>
+          </div>
+
+          <div class="row mt-3">
+            <div class="media">
+              <img src="img/img-26.jpg" class="img-fluid" alt="media-image">
+              <div class="media-body ml-2">
+                <h6>Tin chuyển nhượng 30/5</h6>
+              </div>
+            </div>
+          </div>
+          </div>
+
+          <div class="col-md-3 pl-4">
+            <div class="row">
+            <h5>Liên hệ</h5>
+          </div>
+          <div class="row mb-4">
+          <div class="underline bg-light" style="width: 50px;"></div>
+          </div>
+          <div class="row">
+            <address><span style="color:#777" class="fa fa-map-marker"></span> &nbsp;TIUH - Số xx, phường XX, Quận XX, TP.HCM</address>
+					  <div class="phone-footer"><span style="color:#777" class="fa fa-phone"></span> &nbsp;Hotline: 1800.xxxx (miễn phí)</div>
+					  <div class="phone-footer"><img src="//bizweb.dktcdn.net/100/108/842/themes/692783/assets/zalo.png?1591004780266" alt="zalo1"/> &nbsp;Zalo bán lẻ: xxxxxxxxxx</div>
+					  <div class="phone-footer"><img src="//bizweb.dktcdn.net/100/108/842/themes/692783/assets/zalo.png?1591004780266" alt="zalo2"/> &nbsp;<a>Zalo bán sỉ: xxxxxxxxxx</a> </div>
+          </div>
+          </div>
+
+          <div class="col-md-3">
+            <div class="row">
+            <h5>Tags</h5>
+          </div>
+          <div class="row mb-4">
+          <div class="underline bg-light" style="width: 50px;"></div>
+          </div>
+          <button class="btn btn-outline-light">Nike</button> <button class="btn btn-outline-light">Giày đá banh</button> <button class="btn btn-outline-light">Phụ kiện</button> <button class="btn btn-outline-light">Sale</button> <button class="btn btn-outline-light">Adidas</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/js/bootstrap.min.js" integrity="sha384-7aThvCh9TypR7fIc2HV4O/nFMVCBwyIUKL8XCtKE+8xgCgl/PQGuFsvShjr74PBp" crossorigin="anonymous"></script>
 </body>
 </html>
--- register.php
+++ register.php
@@ -1,33 +1,33 @@
 <?php
 // Include config file
 require_once "config.php";
- 
+
 // Define variables and initialize with empty values
 $username = $password = $confirm_password = "";
 $username_err = $password_err = $confirm_password_err = "";
- 
+
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
- 
+
     // Validate username
     if(empty(trim($_POST["username"]))){
         $username_err = "Please enter a username.";
     } else{
         // Prepare a select statement
         $sql = "SELECT id FROM users WHERE username = ?";
-        
+
         if($stmt = $mysqli->prepare($sql)){
             // Bind variables to the prepared statement as parameters
             $stmt->bind_param("s", $param_username);
-            
+
             // Set parameters
             $param_username = trim($_POST["username"]);
-            
+
             // Attempt to execute the prepared statement
             if($stmt->execute()){
                 // store result
                 $stmt->store_result();
-                
+
                 if($stmt->num_rows == 1){
                     $username_err = "This username is already taken.";
                 } else{
@@ -41,40 +41,40 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             $stmt->close();
         }
     }
-    
+
     // Validate password
     if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter a password.";     
+        $password_err = "Please enter a password.";
     } elseif(strlen(trim($_POST["password"])) < 6){
         $password_err = "Password must have atleast 6 characters.";
     } else{
         $password = trim($_POST["password"]);
     }
-    
+
     // Validate confirm password
     if(empty(trim($_POST["confirm_password"]))){
-        $confirm_password_err = "Please confirm password.";     
+        $confirm_password_err = "Please confirm password.";
     } else{
         $confirm_password = trim($_POST["confirm_password"]);
         if(empty($password_err) && ($password != $confirm_password)){
             $confirm_password_err = "Password did not match.";
         }
     }
-    
+
     // Check input errors before inserting in database
     if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
-        
+
         // Prepare an insert statement
         $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
-         
+
         if($stmt = $mysqli->prepare($sql)){
             // Bind variables to the prepared statement as parameters
             $stmt->bind_param("ss", $param_username, $param_password);
-            
+
             // Set parameters
             $param_username = $username;
             $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
-            
+
             // Attempt to execute the prepared statement
             if($stmt->execute()){
                 // Redirect to login page
@@ -87,49 +87,219 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             $stmt->close();
         }
     }
-    
+
     // Close connection
     $mysqli->close();
 }
 ?>
- 
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Sign Up</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
-    </style>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+ <!--bootsstrap cdn-->
+  <link rel="stylesheet"
+  href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css"
+  integrity="sha384-PDle/QlgIONtM1aqA2Qemk5gPOE7wFq8+Em+G/hmo5Iq0CCmYZLv3fVRDJ4MMwEA"
+  crossorigin="anonymous">
+  <!--font awsome cdn-->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
+  integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
+  crossorigin="anonymous">
+  <script type="text/javascript">
+    function toBottom() {
+      window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' })
+    }
+  </script>
+  <link rel="stylesheet" href="css/style.css">
+  <title>Register</title>
 </head>
 <body>
-    <div class="wrapper">
-        <h2>Sign Up</h2>
-        <p>Please fill this form to create an account.</p>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
-                <label>Username</label>
-                <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
-                <span class="help-block"><?php echo $username_err; ?></span>
-            </div>    
-            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-                <label>Password</label>
-                <input type="password" name="password" class="form-control" value="<?php echo $password; ?>">
-                <span class="help-block"><?php echo $password_err; ?></span>
+<!--top bar (pt=padding top ,pb=padding bottom) -->
+    <div class="container-fluid bg-dark header-top d-md-block d-none">
+        <div class="container">
+            <div class="row text-light pt-2 pb-2">
+                <div class="col-md-5">
+                    <i class="far fa-envelope" aria-hidden="true"></i>
+                    tiuh@tiuh.corp
+                </div>
+                <div class="col-md-3"></div>
+                <div class="col-md-2"><i class="fa fa-user" aria-hidden="true"> </i> <a style="color: white; text-decoration: none;" href="login.html"> Tài khoản </a></div>
+                <div class="col-md-2"><i class="fa fa-cart-plus" aria-hidden="true"> </i> <a style="color: white; text-decoration: none;" href="buy.html"> Giỏ hàng </a></div>
             </div>
-            <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
-                <label>Confirm Password</label>
-                <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
-                <span class="help-block"><?php echo $confirm_password_err; ?></span>
+        </div>
+    </div>
+
+
+<!--nav bar-->
+<div class="container-fluid bg-black">
+
+    <nav class="container navbar navbar-expand-lg navbar-dark bg-black">
+            <a class="navbar-brand" href="index.html"><h1>Tiuh</h1></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="index.html">Trang chủ <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="intro.html">Giới thiệu</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Sản Phẩm
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="shoes.html">Giày</a>
+                    <a class="dropdown-item" href="accessories.html">Phụ Kiện</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="sale.html">Giảm giá</a>
+                  </div>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="javascript:toBottom()">Liên hệ</a>
+                </li>
+              </ul>
+              <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+              </form>
             </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Submit">
-                <input type="reset" class="btn btn-default" value="Reset">
+          </nav>
+</div>
+
+
+  <div class="container-fluid bg-light-gray">
+
+  <div class="container pt-5">
+
+    <div class="row">
+      <h3>ĐĂNG KÝ</h3>
+    </div>
+      <div class="row">
+      <div class="underline"></div>
+    </div>
+  </div>
+  <div class="container pt-5">
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+            <label>Username</label>
+            <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
+            <span class="help-block"><?php echo $username_err; ?></span>
+        </div>
+        <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+            <label>Password</label>
+            <input type="password" name="password" class="form-control" value="<?php echo $password; ?>">
+            <span class="help-block"><?php echo $password_err; ?></span>
+        </div>
+        <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+            <label>Confirm Password</label>
+            <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
+            <span class="help-block"><?php echo $confirm_password_err; ?></span>
+        </div>
+        <div class="form-group">
+            <input type="submit" class="btn btn-primary" value="Submit">
+            <input type="reset" class="btn btn-default" value="Reset">
+        </div>
+        <p>Already have an account? <a href="login.php">Login here</a>.</p>
+    </form>
+  </div>
+
+  </div>
+
+
+
+
+
+  <footer>
+    <div class="container-fluid pt-5 pb-5 bg-dark text-light">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3">
+          <div class="row">
+            <h5>Thử nghiệm</h5>
+          </div>
+          <div class="row mb-4">
+          <div class="underline bg-light" style="width: 50px;"></div>
+          </div>
+
+              <p><i class="fa fa-chevron-right" aria-hidden="true"></i> Đăng kí</p>
+              <p><i class="fa fa-chevron-right" aria-hidden="true"></i> Đăng nhập</p>
+              <p><i class="fa fa-chevron-right" aria-hidden="true"></i> Bảo mật</p>
+              <p><i class="fa fa-chevron-right" aria-hidden="true"></i> Bình luận</p>
+              <p><i class="fa fa-chevron-right" aria-hidden="true"></i> Hướng dẫn</p>
+
+          </div>
+
+
+          <div class="col-md-3">
+            <div class="row">
+            <h5>Bài đăng gần nhất</h5>
+          </div>
+          <div class="row mb-4">
+          <div class="underline bg-light" style="width: 50px;"></div>
+          </div>
+          <div class="row">
+            <div class="media">
+              <img src="img/img-25.jpg" class="img-fluid" alt="media-image">
+              <div class="media-body ml-2">
+                <h6>Joshua Kimmich. Cầu thủ tất cả trong một!</h6>
+              </div>
             </div>
-            <p>Already have an account? <a href="login.php">Login here</a>.</p>
-        </form>
-    </div>    
+          </div>
+
+          <div class="row mt-3">
+            <div class="media">
+              <img src="img/img-26.jpg" class="img-fluid" alt="media-image">
+              <div class="media-body ml-2">
+                <h6>Tin chuyển nhượng 30/5</h6>
+              </div>
+            </div>
+          </div>
+          </div>
+
+          <div class="col-md-3 pl-4">
+            <div class="row">
+            <h5>Liên hệ</h5>
+          </div>
+          <div class="row mb-4">
+          <div class="underline bg-light" style="width: 50px;"></div>
+          </div>
+          <div class="row">
+            <address><span style="color:#777" class="fa fa-map-marker"></span> &nbsp;TIUH - Số xx, phường XX, Quận XX, TP.HCM</address>
+					  <div class="phone-footer"><span style="color:#777" class="fa fa-phone"></span> &nbsp;Hotline: 1800.xxxx (miễn phí)</div>
+					  <div class="phone-footer"><img src="//bizweb.dktcdn.net/100/108/842/themes/692783/assets/zalo.png?1591004780266" alt="zalo1"/> &nbsp;Zalo bán lẻ: xxxxxxxxxx</div>
+					  <div class="phone-footer"><img src="//bizweb.dktcdn.net/100/108/842/themes/692783/assets/zalo.png?1591004780266" alt="zalo2"/> &nbsp;<a>Zalo bán sỉ: xxxxxxxxxx</a> </div>
+          </div>
+          </div>
+
+          <div class="col-md-3">
+            <div class="row">
+            <h5>Tags</h5>
+          </div>
+          <div class="row mb-4">
+          <div class="underline bg-light" style="width: 50px;"></div>
+          </div>
+          <button class="btn btn-outline-light">Nike</button> <button class="btn btn-outline-light">Giày đá banh</button> <button class="btn btn-outline-light">Phụ kiện</button> <button class="btn btn-outline-light">Sale</button> <button class="btn btn-outline-light">Adidas</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/js/bootstrap.min.js" integrity="sha384-7aThvCh9TypR7fIc2HV4O/nFMVCBwyIUKL8XCtKE+8xgCgl/PQGuFsvShjr74PBp" crossorigin="anonymous"></script>
 </body>
 </html>
