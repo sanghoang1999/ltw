d78fdd8062eb895a8df1f492a6ae350d785f69d8
tuhuynh23 <tu.huynh.23@hcmut.edu.vn>
2020-07-01 22:10:15 +0700

    comment

--- index.php
+++ index.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-   <!--bootsstrap cdn-->
+   <!--bootstrap cdn-->
     <link rel="stylesheet"
     href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css"
     integrity="sha384-PDle/QlgIONtM1aqA2Qemk5gPOE7wFq8+Em+G/hmo5Iq0CCmYZLv3fVRDJ4MMwEA"
     crossorigin="anonymous">
-    <!--font awsome cdn-->
+    <!--font awesome cdn-->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
     integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
     crossorigin="anonymous">
